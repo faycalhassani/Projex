611ac28f3dfd54f262f02d0b0ae37afddd8af5b0
Faycal HASSANI <fhassani@live.ca>
2015-11-29 13:25:44 -0500

    Create new ASP.NET 5 application

---
+++ src/Projex/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Projex.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
